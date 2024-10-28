a12271c1639eb5a55b2da2356652804c667980d3
safwan203 <safwansajjad15@gmail.com>
2024-10-28 09:54:25 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+// function add(){
+//     let num1=10
+//     let num2=40
+//     let result=num1+num2
+//     alert(result)
+// }
+
+
+let name1 = prompt('enter your Name')
+let name2 = prompt('enter your partner Name')
+
+let num =Math.floor(Math.random() * 100) +1
+alert(num)
+// console.log(num);
